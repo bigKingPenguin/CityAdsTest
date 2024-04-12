8f843160973b957d9b10462282fb682e90c20ff9
Emiliia <emiliatoropova@yandex.ru>
2024-04-12 14:04:20 +0300

    remove autoimports, add images

--- components/common/PageHeader.vue
+++ components/common/PageHeader.vue
@@ -3,11 +3,24 @@
 </script>
 
 <template>
-  <header>
-erfewrferw
+  <header class="header">
+    <nuxt-icon name="logo" filled class="header__logo"/>
   </header>
 </template>
 
 <style scoped lang="scss">
 
+  .header {
+    padding: var(--space-sections) var(--body-padding);
+
+    &__logo {
+      display: block;
+      width: fit-content;
+
+      &:deep(svg) {
+        width: 180px;
+        height: 38px;
+      }
+    }
+  }
 </style>
